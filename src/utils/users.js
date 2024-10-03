e3f0ec3acb0d110505420efac1e8578ef4a4e70e
KimKyuHyeok <kyuhyeok@naver.com>
2024-10-03 22:11:08 +0900

    fix: 유효성 검사 수정

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,14 +1,14 @@
 const users = [];
 
 const addUser = ({id, username, room}) => {
-    username = username.trim().toLowerCase();
-    room = room.trim().toLowerCase();
-
     if(!username || !room) {
         return {
             error: '사용자 이름과 방이 필요합니다.'
         }
     }
+
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
 
     const existingUser = users.find((user) => {
         return user.room === room && user.username === username;
